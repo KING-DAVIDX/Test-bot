265711678fb7e21c588caf50c1e4849409347746
KING DAVID <xking6030@gmail.com>
2025-09-17 02:10:06 +0100

    Add files via upload

--- start.js
+++ start.js
@@ -1,9 +1,11 @@
 import chalk from "chalk";
-import { start } from "./lib/index.js";
 
-console.log(chalk.greenBright("[ Nexus ] is active ✅"));
-
-start().catch(err => {
+let start;
+try {
+  ({ start } = await import("./lib/index.js"));
+  await start();
+  console.log(chalk.greenBright("[ Nexus ] is active ✅"));
+} catch (err) {
   console.error(chalk.red("❌ Bot failed to start:"));
   console.error(err && err.stack ? err.stack : err);
-});
+}
