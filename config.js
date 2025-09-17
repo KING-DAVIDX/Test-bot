0c150e097741b13f5d6cf7d126ddc3473602647e
KING DAVID <xking6030@gmail.com>
2025-09-17 12:01:34 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export default {
-  SESSION: process.env.SESSION || "Nexus_OBCbSJJO",
-  PREFIX: process.env.PREFIX || "."
+  SESSION: process.env.SESSION || "Nexus_6d1IcjA9",
+  PREFIX: process.env.PREFIX || ".",
+  START_UP_MSG: process.env.START_UP_MSG || true
 }
