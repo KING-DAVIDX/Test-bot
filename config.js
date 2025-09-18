632829ccf121171ea19dd9b3d4c06e7dfedd03e8
KING DAVID <xking6030@gmail.com>
2025-09-18 13:10:21 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
   SESSION: process.env.SESSION || "Nexus_8cjTlVBY",
   PREFIX: process.env.PREFIX || ".",
   START_UP_MSG: process.env.START_UP_MSG || true,
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "2348100835767",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2348100835797",
   SUDO: process.env.SUDO || "",
   MODE: process.env.MODE || "private"
 }
