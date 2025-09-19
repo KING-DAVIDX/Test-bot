1df6d813b402f4a123f4863a4b0ce6d519118e5d
KING DAVID <xking6030@gmail.com>
2025-09-19 15:24:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ export default {
   START_UP_MSG: process.env.START_UP_MSG || true,
   OWNER_NUMBER: process.env.OWNER_NUMBER || "2349168878717",
   SUDO: process.env.SUDO || "",
-  MODE: process.env.MODE || "private"
+  MODE: process.env.MODE || "public"
 }
